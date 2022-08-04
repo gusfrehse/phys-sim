cc8f88df9ed3dfbab0861978b0cb6bd14ebd2150
Gustavo S Frehse <gusfrehse@gmail.com>
2022-08-04 20:50:36 -0300

    Finished swapchain creation

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL_vulkan.h>
 
+#include <algorithm>
+#include <cstdint>
 #include <cstdio>
+#include <limits>
 #include <optional>
 
 #include <vulkan/vulkan.hpp>
@@ -34,6 +37,7 @@ struct window_and_vulkan_state {
 
   vk::SwapchainKHR swapchain;
   vk::Format swapchain_image_format;
+  vk::Extent2D swapchain_image_extent;
 
   std::vector<vk::Image> swapchain_images;
   std::vector<vk::ImageView> swapchain_image_views;
@@ -43,28 +47,104 @@ struct window_and_vulkan_state {
   vk::Queue queue;
 
   auto create_swapchain() -> auto{
-    auto surface_capabilities = physical_device.getSurfaceCapabilitiesKHR(surface);
+    fprintf(stderr, "Creating swapchain\n");
+    auto surface_capabilities =
+        physical_device.getSurfaceCapabilitiesKHR(surface);
     auto surface_formats = physical_device.getSurfaceFormatsKHR(surface);
-    auto surface_present_modes = physical_device.getSurfacePresentModesKHR(surface);
+    auto surface_present_modes =
+        physical_device.getSurfacePresentModesKHR(surface);
 
-    // choose format, try to find SRGB, if failed choose the first available 
+    // choose format, try to find SRGB, if failed choose the first available
     vk::SurfaceFormatKHR chosen_format = surface_formats[0];
-    for (auto& format : surface_formats) {
-      if (format.format == vk::Format::eB8G8R8A8Srgb && format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
+    for (auto &format : surface_formats) {
+      if (format.format == vk::Format::eB8G8R8A8Srgb &&
+          format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
         chosen_format = format;
       }
     }
 
+    swapchain_image_format = chosen_format.format;
+
     // choose present mode, try to find immediate, if failed choose FIFO
     vk::PresentModeKHR chosen_present_mode = vk::PresentModeKHR::eFifo;
-    for (auto& present_mode : surface_present_modes) {
+    for (auto &present_mode : surface_present_modes) {
       if (present_mode == vk::PresentModeKHR::eImmediate) {
         chosen_present_mode = vk::PresentModeKHR::eImmediate;
       }
     }
 
-    // TODO swap extent
+    // find swapchain extent, for some reason it seems to need to check if the
+    // current is equal to max_int and only then we actually change it???
+    vk::Extent2D chosen_extent = surface_capabilities.currentExtent;
+    if (surface_capabilities.currentExtent.width ==
+        std::numeric_limits<uint32_t>::max()) {
+      SDL_GL_GetDrawableSize(window, (int *)&chosen_extent.width,
+                             (int *)&chosen_extent.height);
 
+      chosen_extent.width = std::clamp(
+          chosen_extent.width, surface_capabilities.minImageExtent.width,
+          surface_capabilities.maxImageExtent.width);
+
+      chosen_extent.height = std::clamp(
+          chosen_extent.height, surface_capabilities.minImageExtent.height,
+          surface_capabilities.maxImageExtent.height);
+    }
+
+    swapchain_image_extent = chosen_extent;
+
+    // find the minimum image count + 1 and smaller than maximum image count
+    uint32_t swapchain_image_count = surface_capabilities.minImageCount + 1;
+    if (surface_capabilities.maxImageCount > 0 &&
+        swapchain_image_count > surface_capabilities.maxImageCount) {
+      swapchain_image_count = surface_capabilities.maxImageCount;
+    }
+
+    vk::SwapchainCreateInfoKHR swapchain_create_info{};
+    swapchain_create_info.sType = vk::StructureType::eSwapchainCreateInfoKHR;
+    swapchain_create_info.surface = surface;
+
+    swapchain_create_info.minImageCount = swapchain_image_count;
+    swapchain_create_info.imageFormat = chosen_format.format;
+    swapchain_create_info.imageColorSpace = chosen_format.colorSpace;
+    swapchain_create_info.imageExtent = chosen_extent;
+    swapchain_create_info.imageArrayLayers = 1;
+
+    swapchain_create_info.imageUsage = vk::ImageUsageFlagBits::eColorAttachment;
+    swapchain_create_info.imageSharingMode =
+        vk::SharingMode::eExclusive; // change this for more queue families
+    swapchain_create_info.queueFamilyIndexCount = 0;
+    swapchain_create_info.pQueueFamilyIndices = nullptr;
+    swapchain_create_info.preTransform = surface_capabilities.currentTransform;
+    swapchain_create_info.compositeAlpha =
+        vk::CompositeAlphaFlagBitsKHR::eOpaque;
+    swapchain_create_info.presentMode = chosen_present_mode;
+    swapchain_create_info.clipped = true;
+
+    // create the swapchain
+    swapchain = device.createSwapchainKHR(swapchain_create_info);
+
+    // save the images
+    swapchain_images = device.getSwapchainImagesKHR(swapchain);
+
+    // create the image views
+    for (auto &image : swapchain_images) {
+      vk::ImageViewCreateInfo image_view_create_info{};
+      image_view_create_info.setImage(image);
+      image_view_create_info.setViewType(vk::ImageViewType::e2D);
+      image_view_create_info.setFormat(swapchain_image_format);
+      image_view_create_info.setComponents(
+          {vk::ComponentSwizzle::eIdentity, vk::ComponentSwizzle::eIdentity,
+           vk::ComponentSwizzle::eIdentity, vk::ComponentSwizzle::eIdentity});
+      image_view_create_info.subresourceRange.setAspectMask(vk::ImageAspectFlagBits::eColor);
+      image_view_create_info.subresourceRange.setBaseMipLevel(0);
+      image_view_create_info.subresourceRange.setLevelCount(1);
+      image_view_create_info.subresourceRange.setBaseArrayLayer(0);
+      image_view_create_info.subresourceRange.setLayerCount(1);
+
+      swapchain_image_views.push_back(device.createImageView(image_view_create_info));
+    }
+
+    fprintf(stderr, "Created swapchain\n");
   }
 
   auto init_vulkan() -> auto{
@@ -79,7 +159,7 @@ struct window_and_vulkan_state {
     CHECK_SDL(window, == 0);
 
     // instance creation
-    vk::ApplicationInfo application_info("Aim Trainer");
+    vk::ApplicationInfo application_info("phys sim");
 
     // validation layer work
     std::vector<const char *> validation_layers = {
@@ -176,11 +256,11 @@ struct window_and_vulkan_state {
   }
 
   auto cleanup() -> auto{
-    // device.destroySwapchainKHR(swapchain);
+    for (auto &e : swapchain_image_views) {
+      device.destroyImageView(e);
+    }
 
-    // for (auto &e : swapchain_image_views) {
-    //   device.destroyImageView(e);
-    // }
+    device.destroy(swapchain);
 
     device.destroy();
 
@@ -189,10 +269,7 @@ struct window_and_vulkan_state {
     SDL_DestroyWindow(window);
   }
 
-  auto init() -> auto{
-    init_vulkan();
-    create_swapchain();
-  }
+  auto init() -> auto{ init_vulkan(); }
 };
 
 auto main() -> int {

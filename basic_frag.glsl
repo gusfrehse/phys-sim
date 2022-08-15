4ed5dc0894ecd7c25d7bf5f920c9e05b7d856064
Gustavo S Frehse <gusfrehse@gmail.com>
2022-08-15 13:55:45 -0300

    colored triangle

--- basic_frag.glsl
+++ basic_frag.glsl
@@ -1,7 +1,8 @@
 #version 450
 
+layout (location = 0) in vec3 frag_color;
 layout (location = 0) out vec4 color_out;
 
 void main() {
-  color_out = vec4(1.0, 0.0, 0.0, 1.0);
+  color_out = vec4(frag_color, 1.0);
 }

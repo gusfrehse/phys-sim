0542af2f2b49f80a3119f5c0d96e4674bc919e10
Gustavo S Frehse <gusfrehse@gmail.com>
2022-10-10 17:34:54 -0300

    keyboard input

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
+#include <SDL2/SDL_keycode.h>
+#include <cstdio>
+#include <unordered_map>
+
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
 
 #include "renderer.hpp"
 
 #include "object.hpp"
 
 const uint32_t NUM_OBJECTS = 20;
+
+std::unordered_map<SDL_Keycode, bool> keys;
+
+auto handle_event(SDL_Event e) {
+  switch (e.type) {
+    case SDL_KEYDOWN:
+      keys[e.key.keysym.sym] = 1;
+      break;
+
+    case SDL_KEYUP:
+      keys[e.key.keysym.sym] = 0;
+      break;
+  }
+}
 
 auto main() -> int {
   renderer render;
@@ -15,19 +34,26 @@ auto main() -> int {
 
   // SDL_Delay(1000);
   bool running = true;
-  SDL_Event event;
+
   while (running) {
+    SDL_Event event;
+
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
         running = false;
       }
+
+      handle_event(event);
+    }
+
+    if (keys[SDLK_SPACE]) {
+      printf("space pressed\n");
     }
 
     // update physics
     // TODO: :P
 
     // update uniforms
-    //render.record_command_buffer(render.command_buffers, int image_index)
     render.draw_frame();
   }
 
